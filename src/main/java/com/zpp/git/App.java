1f9dea3ea7d5042746654cc1bd41c811931327c3
zpp <741641339@qq.com>
2022-12-14 10:12:31 +0800

    first commit

--- src/main/java/com/zpp/git/App.java
+++ src/main/java/com/zpp/git/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World! git1111111111" );
         System.out.println( "Hello World! git2222222222" );
         System.out.println( "Hello World! hot-fix111111" );
+        System.out.println( "Hello World! hot-fix222222" );
     }
 }
